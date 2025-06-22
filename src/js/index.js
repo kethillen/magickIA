71a2c90ffd75febc04689827dbcf88a533c36c22
Kethillen <kethillen123barboza@gmail.com>
2025-06-22 18:57:56 -0300

    Finalizando alterações locais

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,11 @@
-/*
-  O que precisamos fazer? - Quando o usuário clicar no botão "Aplicar filtros", vamos filtrar as cartas baseado na categoria e no preço máximo selecionados
-    OBJETIVO 1 - Criar a funcionalidade de filtrar as cartas
-        passo 1 - pegar o botao de aplicar filtros  do HTML e mandar pro JS
-        passo 2 - escutar o clique no botão de aplicar filtros
-        passo 3 - pegar os valores dos campos de categoria e preço
-        passo 4 - para cada carta, verificar se ela deve ser mostrada ou escondida baseado ns filtros que a pessoa digitou 
-*/
-
-// passo 1 - pegar o botao de aplicar filtros  do HTML e mandar pro JS:
 const botaoFiltrar = document.querySelector('.btn-filtrar');
 
-// passo 2 - escutar o clique no botão de aplicar filtros:
+
 botaoFiltrar.addEventListener('click', function(){
     console.log('clicou no botão filtrar');
-//passo 3 - pegar os valores dos campos de categoria e preço
+
 const categoriaSelecionada = document.querySelector('#categoria').value;
 const precoMaximoSelecionado = document.querySelector('#preco').value;
-//passo 4 - para cada carta, verificar se ela deve ser mostrada ou escondida baseado ns filtros que a pessoa digitou
 const cartas = document.querySelectorAll('.carta')
 
   cartas.forEach(function (carta) {
